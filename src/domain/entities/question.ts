b68f74de14215e345f4bb56fd6e2f118481d4c49
Talles <worktalles@hotmail.com>
2025-07-31 20:04:00 -0300

    chore: create interface to questions

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,24 @@
 import { randomUUID } from "node:crypto"
 
+interface QuestionProps {
+  title: string
+  content: string
+  slug: string
+  authorId: string
+}
+
 export class Question {
   public id: string
   public title: string
   public content: string
+  public slug: string
+  public authorId: string
 
-  constructor(title: string, content: string, id?: string) {
-    this.title = title
-    this.content = content
+  constructor(props: QuestionProps, id?: string) {
+    this.title = props.title
+    this.content = props.content
+    this.slug = props.slug
+    this.authorId = props.authorId
     this.id = id ?? randomUUID()
   }
 }
