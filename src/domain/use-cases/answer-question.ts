28df2b4e76a4dda11220477ddfd1150634a11e2f
Talles <worktalles@hotmail.com>
2025-07-31 20:34:39 -0300

    fix: get content as a object

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -10,7 +10,7 @@ interface AnswerQuestionUseCaseRequest {
 export class AnswerQuestionUseCase {
   constructor (private answersRepository: AnswersRepository) {}
   async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer(content)
+    const answer = new Answer({ content })
 
     await this.answersRepository.create(answer)
 
