96e7571a9d93afd147cbd817f0c60c3874d92e61
Talles <worktalles@hotmail.com>
2025-07-31 20:50:53 -0300

    chore: use unique id

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,15 +1,16 @@
 import { randomUUID } from "node:crypto"
+import { UniqueEntityId } from "./unique-entity-id"
 
 export class Entity<Props> {
-  private _id: string
+  private _id: UniqueEntityId
   protected props: Props
-  
+
   get id() {
     return this._id
   }
 
   constructor(props: Props, id?: string) {
-    this._id = id ?? randomUUID()
+    this._id = new UniqueEntityId(id)
     this.props = props
   }
 }
