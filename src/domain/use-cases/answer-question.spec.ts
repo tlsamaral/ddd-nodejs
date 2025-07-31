8c7b953f658d6685bb65cd797066089639782c24
Talles <worktalles@hotmail.com>
2025-07-31 20:00:43 -0300

    chore: create answers test

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,16 @@
 import { expect, test } from "vitest"
 import { AnswerQuestionUseCase } from "./answer-question"
+import { AnswersRepository } from "../repositories/answers-repository"
+import { Answer } from "../entities/answer"
 
-test('create answer question', () => {
-  const answerQuestionUseCase = new AnswerQuestionUseCase()
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {}
+}
+
+test('create answer question', async () => {
+  const answerQuestionUseCase = new AnswerQuestionUseCase(fakeAnswersRepository)
   
-  const answer = answerQuestionUseCase.execute({
+  const answer = await answerQuestionUseCase.execute({
     instructorId: '1',
     questionId: '1',
     content: 'content'
