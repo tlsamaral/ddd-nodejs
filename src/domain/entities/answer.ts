d67ab6d2d4b24bcfa885d1fdf6a529a219d84117
Talles <worktalles@hotmail.com>
2025-07-31 21:01:55 -0300

    chore: adding more fields

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,6 +1,11 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 interface AnswerProps {
+  authorId: UniqueEntityId
+  questionId: UniqueEntityId
   content: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
