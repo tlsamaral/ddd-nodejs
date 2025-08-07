24a51a7dace22a079e9d9ab7dfc761fa82d3a41d
Talles <worktalles@hotmail.com>
2025-08-07 19:30:36 -0300

    chore: update parameters

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -3,15 +3,19 @@ import { Answer } from "../entities/answer"
 import { AnswersRepository } from "../repositories/answers-repository"
 
 interface AnswerQuestionUseCaseRequest {
-  instructorId: UniqueEntityId
-  questionId: UniqueEntityId
+  instructorId: string
+  questionId: string
   content: string
 }
 
 export class AnswerQuestionUseCase {
   constructor (private answersRepository: AnswersRepository) {}
   async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({ content })
+    const answer = Answer.create({ 
+      content, 
+      authorId: new UniqueEntityId(instructorId), 
+      questionId: new UniqueEntityId(questionId)
+    })
 
     await this.answersRepository.create(answer)
 
