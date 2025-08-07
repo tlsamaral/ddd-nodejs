d9c9b1ff4793786633a496362e48caf2553f7b31
Talles <worktalles@hotmail.com>
2025-08-07 19:31:00 -0300

    chore: use new structure

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,8 +1,17 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 interface InstructorProps {
   name: string
 }
 
 export class Instructor extends Entity<InstructorProps> {
+  static create(
+    props: InstructorProps, 
+    id?: UniqueEntityId
+  ) {
+    const answer = new Instructor(props, id)
+
+    return answer
+  }
 }
--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -1,8 +1,17 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 interface StudentProps {
   name: string
 }
 
 export class Student extends Entity<StudentProps> {
+  static create(
+    props: StudentProps, 
+    id?: UniqueEntityId
+  ) {
+    const answer = new Student(props, id)
+
+    return answer
+  }
 }
