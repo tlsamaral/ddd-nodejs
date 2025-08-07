3c3f0747ad47fab6a02a39852948e8dea20d8410
Talles <worktalles@hotmail.com>
2025-08-07 19:48:03 -0300

    chore: create setters to content, title and best answer

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -51,12 +51,33 @@ export class Question extends Entity<QuestionProps> {
     return this.content.substring(0, 120).trimEnd().concat('...')
   }
 
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    this.touch()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
+
   static create(
-    props: Optional<QuestionProps, 'createdAt'>, 
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>, 
     id?: UniqueEntityId
   ) {
     const question = new Question({
       ...props,
+      slug: props.slug ?? Slug.createFromText(props.title),
       createdAt: props.createdAt ?? new Date(),
     }, id)
 
