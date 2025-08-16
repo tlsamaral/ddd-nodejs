5b2ef6e55a77a52e2fbaa6847b59c762e96c0c84
Talles <worktalles@hotmail.com>
2025-08-16 16:47:40 -0300

    chore: use sut named

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -2,16 +2,18 @@ import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questio
 import { CreateQuestionUseCase } from './create-question'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
-let createQuestion: CreateQuestionUseCase
+let sut: CreateQuestionUseCase
 
 describe('Create question', () => {
   beforeEach(() => {
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
-    createQuestion = new CreateQuestionUseCase(inMemoryQuestionsRepository)
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
   })
 
+  // sut = system under test
+
   it('should be able to create a new question', async () => {
-    const { question } = await createQuestion.execute({
+    const { question } = await sut.execute({
       authorId: '1',
       title: 'title',
       content: 'content',
