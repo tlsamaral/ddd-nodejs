18be039f365cf6a9b17d811e72e62e134b84902d
Talles <worktalles@hotmail.com>
2025-08-16 16:35:12 -0300

    chore: create a test to questions

---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,19 @@
+import type { Question } from '../../enterprise/entities/question'
+import type { QuestionsRepository } from '../repositories/questions-repository'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  create: async (question: Question) => {},
+}
+
+test('create a question', async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
+
+  const { question } = await createQuestion.execute({
+    authorId: '1',
+    title: 'title',
+    content: 'content',
+  })
+
+  expect(question.id).toBeTruthy()
+})
