15ba1050a9f4582331036e3a97c28c2c2b570ba2
Talles <worktalles@hotmail.com>
2025-11-02 20:50:42 -0300

    .

---
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -0,0 +1,29 @@
+import type { AnswerComment } from '../../enterprise/entities/answer-comment'
+import type { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+
+interface FetchAnswerCommentsUseCaseRequest {
+  answerId: string
+  page: number
+}
+
+type FetchAnswerCommentsUseCaseResponse = {
+  answerComments: AnswerComment[]
+}
+
+export class FetchAnswerCommentsUseCase {
+  constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
+
+  async execute({
+    answerId,
+    page,
+  }: FetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
+    const answerComments =
+      await this.answerCommentsRepository.findManyByAnswerId(answerId, {
+        page,
+      })
+
+    return {
+      answerComments,
+    }
+  }
+}
