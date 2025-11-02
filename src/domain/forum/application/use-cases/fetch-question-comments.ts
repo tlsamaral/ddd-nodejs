a80cf7af970d9eeb10ad55b089b0b40eed29bf82
Talles <worktalles@hotmail.com>
2025-11-02 20:51:15 -0300

    .

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -0,0 +1,29 @@
+import type { QuestionComment } from '../../enterprise/entities/question-comment'
+import type { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+
+interface FetchQuestionCommentsUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+type FetchQuestionCommentsUseCaseResponse = {
+  questionComments: QuestionComment[]
+}
+
+export class FetchQuestionCommentsUseCase {
+  constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
+    const questionComments =
+      await this.questionCommentsRepository.findManyByQuestionId(questionId, {
+        page,
+      })
+
+    return {
+      questionComments,
+    }
+  }
+}
