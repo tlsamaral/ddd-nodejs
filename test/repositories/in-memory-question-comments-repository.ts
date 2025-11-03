883d91ef33fd985b5f168e4ab46dd389ee88c9b3
Talles <worktalles@hotmail.com>
2025-11-02 22:35:19 -0300

    .

---
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -0,0 +1,39 @@
+import type { PaginationParams } from '@/core/repositories/pagination-params'
+import type { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
+import type { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+
+export class InMemoryQuestionCommentsRepository
+  implements QuestionCommentsRepository
+{
+  public items: QuestionComment[] = []
+
+  async findById(id: string): Promise<QuestionComment | null> {
+    const questionComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!questionComment) {
+      return null
+    }
+
+    return questionComment
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return questionComments
+  }
+
+  async delete(questionComment: QuestionComment): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
+  }
+
+  async create(questionComment: QuestionComment): Promise<void> {
+    this.items.push(questionComment)
+  }
+}
