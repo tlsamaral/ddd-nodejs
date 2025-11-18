4718e739ad3ed609292cb076cb6a794ad17c6d6d
Talles <worktalles@hotmail.com>
2025-11-18 12:22:16 -0300

    update

---
+++ src/domain/forum/application/repositories/answer-attachments-repository.ts
@@ -0,0 +1,7 @@
+import type { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
+
+export type AnswerAttachmentsRepository = {
+  findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]>
+
+  deleteManyByAnswerId(answerId: string): Promise<void>
+}
