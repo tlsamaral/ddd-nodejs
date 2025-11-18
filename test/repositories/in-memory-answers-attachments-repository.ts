b4f38e703203728ea13b8a93719ae90be4a191f7
Talles <worktalles@hotmail.com>
2025-11-18 12:19:23 -0300

    chore: update

---
+++ test/repositories/in-memory-answers-attachments-repository.ts
@@ -0,0 +1,22 @@
+import type { AnswerAttachmentsRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
+import type { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
+
+export class InMemoryAnswerAttachmentsRepository
+  implements AnswerAttachmentsRepository
+{
+  public items: AnswerAttachment[] = []
+
+  async findManyByAnswerId(answerId: string) {
+    const answerAttachments = this.items.filter(
+      (item) => item.answerId.toString() === answerId,
+    )
+
+    return answerAttachments
+  }
+
+  async deleteManyByAnswerId(answerId: string): Promise<void> {
+    this.items = this.items.filter(
+      (item) => item.answerId.toString() !== answerId,
+    )
+  }
+}
