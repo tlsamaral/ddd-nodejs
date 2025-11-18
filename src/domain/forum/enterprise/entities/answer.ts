23bb8ef83a9ded198ae14fa6766a613b3424a724
Talles <worktalles@hotmail.com>
2025-11-18 12:28:37 -0300

    update

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,20 +1,19 @@
-import { Entity } from '@/core/entities/entity'
-import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
+import type { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import type { Optional } from '@/core/types/optional'
+import { AnswerAttachmentList } from '@/domain/forum/enterprise/entities/answer-attachment-list'
+// import { AnswerCreatedEvent } from '@/domain/forum/enterprise/events/answer-created-event'
 
 export interface AnswerProps {
-  authorId: UniqueEntityId
-  questionId: UniqueEntityId
+  authorId: UniqueEntityID
+  questionId: UniqueEntityID
   content: string
+  attachments: AnswerAttachmentList
   createdAt: Date
   updatedAt?: Date
 }
 
-export class Answer extends Entity<AnswerProps> {
-  get content() {
-    return this.props.content
-  }
-
+export class Answer extends AggregateRoot<AnswerProps> {
   get authorId() {
     return this.props.authorId
   }
@@ -23,7 +22,27 @@ export class Answer extends Entity<AnswerProps> {
     return this.props.questionId
   }
 
-  touch() {
+  get content() {
+    return this.props.content
+  }
+
+  get attachments() {
+    return this.props.attachments
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  private touch() {
     this.props.updatedAt = new Date()
   }
 
@@ -32,17 +51,29 @@ export class Answer extends Entity<AnswerProps> {
     this.touch()
   }
 
+  set attachments(attachments: AnswerAttachmentList) {
+    this.props.attachments = attachments
+    this.touch()
+  }
+
   static create(
-    props: Optional<AnswerProps, 'createdAt'>,
-    id?: UniqueEntityId,
+    props: Optional<AnswerProps, 'createdAt' | 'attachments'>,
+    id?: UniqueEntityID,
   ) {
     const answer = new Answer(
       {
         ...props,
+        attachments: props.attachments ?? new AnswerAttachmentList(),
         createdAt: props.createdAt ?? new Date(),
       },
       id,
     )
+
+    const isNewAnswer = !id
+
+    // if (isNewAnswer) {
+    //   answer.addDomainEvent(new AnswerCreatedEvent(answer))
+    // }
 
     return answer
   }
