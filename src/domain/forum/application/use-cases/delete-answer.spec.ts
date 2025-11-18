0f6b8c587a64f8263114090f6b148b7ad0ac4ea3
Talles <worktalles@hotmail.com>
2025-11-18 12:30:09 -0300

    chore: udpate

--- src/domain/forum/application/use-cases/delete-answer.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -1,10 +1,10 @@
-import { DeleteAnswerUseCase } from './delete-answer'
-import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { makeAnswer } from 'test/factories/make-answer'
+import { makeAnswerAttachment } from 'test/factories/make-answer-attachment'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answers-attachments-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
-import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
-import { makeAnswerAttachment } from 'test/factories/make-answer-attachments'
+import { DeleteAnswerUseCase } from './delete-answer'
 
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
