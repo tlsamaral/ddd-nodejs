9cdce10467c6472c5824c26e21072e7952efcc68
Talles <worktalles@hotmail.com>
2025-11-19 10:22:21 -0300

    chore: create notification structure

---
+++ src/domain/notification/application/use-cases/read-notification.spec.ts
@@ -0,0 +1,48 @@
+import { makeNotification } from 'test/factories/make-notification'
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { ReadNotificationUseCase } from './read-notification'
+
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+
+let sut: ReadNotificationUseCase
+
+describe('Read Notification', () => {
+  beforeEach(() => {
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+    sut = new ReadNotificationUseCase(inMemoryNotificationsRepository)
+  })
+
+  it('should be able to read a notification', async () => {
+    const notification = makeNotification()
+
+    inMemoryNotificationsRepository.items.push(notification)
+
+    const result = await sut.execute({
+      recipientId: notification.recipientId.toString(),
+      notificationId: notification.id.toString(),
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryNotificationsRepository.items[0].readAt).toEqual(
+      expect.any(Date),
+    )
+  })
+
+  it('should not be able to read a notification from another user', async () => {
+    const notification = makeNotification({
+      recipientId: new UniqueEntityID('recipient-2'),
+    })
+
+    inMemoryNotificationsRepository.items.push(notification)
+
+    const result = await sut.execute({
+      recipientId: 'recipient-1',
+      notificationId: notification.id.toString(),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+  })
+})
---
+++ src/domain/notification/application/use-cases/read-notification.ts
@@ -0,0 +1,45 @@
+import { type Either, left, right } from '@/core/either'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import type { Notification } from '@/domain/notification/enterprise/entities/notification'
+import type { NotificationsRepository } from '../repositories/notification-repository'
+
+interface ReadNotificationUseCaseRequest {
+  recipientId: string
+  notificationId: string
+}
+
+type ReadNotificationUseCaseResponse = Either<
+  NotAllowedError | ResourceNotFoundError,
+  {
+    notification: Notification
+  }
+>
+
+export class ReadNotificationUseCase {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute({
+    notificationId,
+    recipientId,
+  }: ReadNotificationUseCaseRequest): Promise<ReadNotificationUseCaseResponse> {
+    const notification =
+      await this.notificationsRepository.findById(notificationId)
+
+    if (!notification) {
+      return left(new ResourceNotFoundError())
+    }
+
+    if (recipientId !== notification.recipientId.toString()) {
+      return left(new NotAllowedError())
+    }
+
+    notification.read()
+
+    await this.notificationsRepository.save(notification)
+
+    return right({
+      notification,
+    })
+  }
+}
