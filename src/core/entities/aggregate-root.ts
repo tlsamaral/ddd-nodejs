1e078d9ac7fd2a14d0e24e97a83e4b8c45914ddf
Talles <worktalles@hotmail.com>
2025-11-19 10:36:24 -0300

    chore: add two methods

--- src/core/entities/aggregate-root.ts
+++ src/core/entities/aggregate-root.ts
@@ -1,3 +1,20 @@
+import type { DomainEvent } from '../events/domain-event'
+import { DomainEvents } from '../events/domain-events'
 import { Entity } from './entity'
 
-export abstract class AggregateRoot<Props> extends Entity<Props> {}
+export abstract class AggregateRoot<Props> extends Entity<Props> {
+  private _domainEvents: DomainEvent[] = []
+
+  get domainEvents() {
+    return this._domainEvents
+  }
+
+  protected addDomainEvent(event: DomainEvent): void {
+    this._domainEvents.push(event)
+    DomainEvents.markAggregateForDispatch(this)
+  }
+
+  public clearEvents(): void {
+    this._domainEvents = []
+  }
+}
