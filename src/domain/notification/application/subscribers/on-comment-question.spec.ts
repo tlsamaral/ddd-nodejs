5ea37599b8f2fd5a156b31f2d769f054bcbf7f60
Talles <worktalles@hotmail.com>
2025-11-19 14:28:50 -0300

    feat: create test to valid if comment on question subscriber is valid

---
+++ src/domain/notification/application/subscribers/on-comment-question.spec.ts
@@ -0,0 +1,65 @@
+import { makeQuestion } from 'test/factories/make-question'
+import { makeQuestionComment } from 'test/factories/make-question-comment'
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { waitFor } from 'test/utils/wait-for'
+import type { MockInstance } from 'vitest'
+import {
+  SendNotificationUseCase,
+  type SendNotificationUseCaseRequest,
+  type SendNotificationUseCaseResponse,
+} from '../use-cases/send-notification'
+import { OnCommentQuestion } from './on-comment-question'
+
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+let sendNotificationUseCase: SendNotificationUseCase
+
+let sendNotificationExecuteSpy: MockInstance<
+  (
+    props: SendNotificationUseCaseRequest,
+  ) => Promise<SendNotificationUseCaseResponse>
+>
+
+describe('On Comment Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
+
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+
+    sendNotificationUseCase = new SendNotificationUseCase(
+      inMemoryNotificationsRepository,
+    )
+
+    sendNotificationExecuteSpy = vi.spyOn(sendNotificationUseCase, 'execute')
+
+    new OnCommentQuestion(inMemoryQuestionsRepository, sendNotificationUseCase)
+  })
+
+  it('should send a notification when question receives new comment', async () => {
+    const question = makeQuestion()
+    const questionComment = makeQuestionComment({
+      questionId: question.id,
+    })
+
+    inMemoryQuestionsRepository.create(question)
+
+    inMemoryQuestionCommentsRepository.create(questionComment)
+
+    await waitFor(() => {
+      expect(sendNotificationExecuteSpy).toHaveBeenCalled()
+    })
+  })
+})
